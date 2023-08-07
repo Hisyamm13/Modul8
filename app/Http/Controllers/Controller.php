eb91bf767a4d4dc7dd9e437d2e9917c5f5a2efb4
hisyam <hisyammahendrap27@gmail.com>
2023-08-07 21:12:01 +0700

    inisialisasi awal

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
+
+Alert::alert('Title','Message', 'Type');
+
+
+// jj
---
+++ database/seeders/EmployeeSedeer.php
@@ -0,0 +1,1 @@
+Employee::factory()->count(10)->create();
